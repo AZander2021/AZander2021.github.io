55c2eb81c039fcc608191db6060045553af90c07
AZander2021 <98670231+AZander2021@users.noreply.github.com>
2022-01-30 00:03:53 +0100

    Update index.js

--- web-app/index.js
+++ web-app/index.js
@@ -6,10 +6,6 @@ var characteristics = {
   password: '13333333-3333-3333-3333-333333330003' 
 };
 
-var toppingsEls = document.getElementById('register');
-var username = document.getElementById('username');
-var password = document.getElementById('password');
-
 // store characteristics after retrieval
 var cachedCharacteristics = {};
 
@@ -28,61 +24,10 @@ var readyCM = function() {
     communityMirrorServer = server;
     return server.getPrimaryService(communityMirrorServiceUUID);
 
-  }).then(function(service) {
-    return Promise.all(Object.values(characteristics).map((uuid)=>service.getCharacteristic(uuid)));
-
-  }).then(function(characteristicObjs) {
-    Object.keys(characteristics).forEach((name, i)=> {
-      cachedCharacteristics[name] = characteristicObjs[i];
-    });
-
   }).catch(function(err) {
     alert('communitymirror (bluetooth) error');
     throw err;
   });
-};
-
-// characteristic setup
-var readyIDReq = function(toppings) {
-  var iDReq = new Uint8Array(1);
-  iDReq[0] = toppings;
-
-  var idRequestCharacteristic = cachedCharacteristics['idrequest'];
-  if(idRequestCharacteristic == null) throw new Error('idrequestcharacteristic not found');
-  return idRequestCharacteristic.writeValue(iDReq).catch(function(err) {
-    alert('idrequest error');
-    throw err;
-  });
-};
-
-var readyUsername = function(username) {
-  var usernameCharacteristic = cachedCharacteristics['username'];
-  if(usernameCharacteristic == null) throw new Error('cant find usernamecharacterisitic!');
-
-  return usernameCharacteristic.writeValue(username);
-};
-
-var readyPassword = function(password) {
-  var usernameCharacteristic = cachedCharacteristics['password'];
-  if(usernameCharacteristic == null) throw new Error('cant find passwordcharacterisitic');
-
-  return usernameCharacteristic.writeValue(password);
-};
-
-//get values
-var getIDReq = function() {
-  if (toppingsEls.checked) return 2
-  else return 1
-};
-
-var getUsername = function() {
-  enc = new TextEncoder();
-  return enc.encode("." + username.value);
-};
-
-var getPassword = function() {
-  enc = new TextEncoder();
-  return enc.encode("." + password.value);
 };
 
 // button listeners
@@ -95,14 +40,3 @@ var onStartButtonClick = function(e) {
     alert('Connection successful!');
   });
 };
-
-var onLoginButtonClick = function(e) {
-  if(communityMirrorServer == null || !communityMirrorServer.connected) {
-    alert('Not connected!');
-    return;
-  }
-
-  readyIDReq(getIDReq())
-      .then(() => readyUsername(getUsername()))
-      .then(() => readyPassword(getPassword()))
-});
